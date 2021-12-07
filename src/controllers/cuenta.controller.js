f6139be5a12b08a38e65ad99334c8540771ecc7e
Armando Prado <armap1999@hotmail.com>
2021-12-06 18:14:43 -0600

    /feat: Was added logIn function and one Middleware

--- src/controllers/cuenta.controller.js
+++ src/controllers/cuenta.controller.js
@@ -1,3 +1,4 @@
+const jwt = require("jsonwebtoken");
 const Cuenta = require("../models/Cuenta");
 
 module.exports.getAllCounts = async function (req, res) {
@@ -19,20 +20,49 @@ module.exports.addCount = async function (req, res) {
   } else {
     res.status(400).json({ message: "Y tu correo we?" });
   } */
-  try{
+  try {
     const cuenta = await Cuenta.create({
-        Nombre: name,
-        Correo: email,
-        Contrasena: password,
-        Direccion: address,
-        Municipio: city,
-        Estatus: 1,
-        FechaAlta: new Date()
-    })
-    res.status(201).json({message: cuenta})
+      Nombre: name,
+      Correo: email,
+      Contrasena: password,
+      Direccion: address,
+      Municipio: city,
+      Estatus: 1,
+      FechaAlta: new Date(),
+    });
+    res.status(201).json({ message: cuenta });
+  } catch (err) {
+    res.status(500).json({ message: "algo salio mal" });
   }
-  catch (err) {
-    res.status(500).json({message: "algo salio mal"})
+};
+
+module.exports.logIn = async function (req, res) {
+  const { email, password } = req.body;
+  try {
+    const cuenta = await Cuenta.findOne({
+      where: { Correo: email, Contrasena: password },
+    });
+    if (cuenta !== undefined) {
+      const token = jwt.sign(
+        {
+          data: {
+            email,
+            id: cuenta.id,
+          },
+        },
+        "Workick",
+        { expiresIn: "1h" }
+      );
+      res.status(200).json({ok: true,
+        token: token,
+        id: cuenta.Id,
+        nombre: cuenta.Nombre,
+        estatus: cuenta.Estatus,
+      })
+    } else {
+      res.status(400).json({ message: "Datos invalidos" });
+    }
+  } catch (err) {
+    res.status(500).json({ message: "Server internal error", error: err });
   }
-  
 };
--- src/routes/cuenta.routes.js
+++ src/routes/cuenta.routes.js
@@ -1,8 +1,15 @@
-const router = require('express').Router();
-const {getAllCounts,addCount,getCoutById} = require('../controllers/cuenta.controller')
+const router = require("express").Router();
+const {
+  getAllCounts,
+  addCount,
+  getCoutById,
+  logIn,
+} = require("../controllers/cuenta.controller");
+const { isAuthorized } = require("../middlewares/isAuthorized");
 
-router.get('/', getAllCounts)
-router.post('/',addCount)
-router.get('/:id',getCoutById)
+router.get("/", [isAuthorized], getAllCounts);
+router.post("/", addCount);
+router.post("/login", logIn);
+router.get("/:id", getCoutById);
 
-module.exports = router
+module.exports = router;
