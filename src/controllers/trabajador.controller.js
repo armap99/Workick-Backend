a8c3808bc3598838004354eaec2424af55eead87
Armando Prado <luis.prado@hemoeco.com>
2021-12-29 14:05:45 -0600

    feat: Worker contoller finished

--- src/controllers/trabajador.controller.js
+++ src/controllers/trabajador.controller.js
@@ -1,23 +1,107 @@
 const Trabajador = require("../models/Trabajador");
-const Cuenta = require("../models/Cuenta")
+const Cuenta = require("../models/Cuenta");
 
-module.exports.getAllWorkers = async function (req,res){
-    try{
-        const worker = await Trabajador.findAll()
-        res.status(200).json(worker);
-    }catch(err){
-        console.log(err)
-        res.status(500).json({ message: "Server internal error", error: err });
-    }
-}
+module.exports.getAllWorkers = async function (req, res) {
+  try {
+    const worker = await Trabajador.findAll();
+    res.status(200).json(worker);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Server internal error", error: err });
+  }
+};
 
 module.exports.getWorkerInfoByUserId = async function (req, res) {
   try {
     const { id } = req.params;
-    const worker = await Cuenta.findOne({ where: { Id: id }, include: [{
-        model: Trabajador,
-    }] });
+    const worker = await Cuenta.findOne({
+      where: { Id: id },
+      include: [
+        {
+          model: Trabajador,
+        },
+      ],
+    });
     res.status(200).json(worker);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Server internal error", error: err });
+  }
+};
+
+module.exports.addWorkerInformation = async function (req, res) {
+  try {
+    const {
+      idCuenta,
+      descripcionCorta,
+      descripcionLarga,
+      categoria,
+      tituloTrabajo,
+    } = req.body;
+    if (
+      descripcionCorta == "" ||
+      descripcionLarga == "" ||
+      tituloTrabajo == ""
+    ) {
+      return res.status(400).json({ message: "Falta de informacion" });
+    }
+    const worker = await Trabajador.create({
+      IdCuenta: idCuenta,
+      DescripcionCorta: descripcionCorta,
+      DescripcionLarga: descripcionLarga,
+      CalificacionGlobal: 0,
+      CalificacionPrecio: 0,
+      Categoria: categoria,
+      Trabajos: 0,
+      TituloTrabajo: tituloTrabajo,
+    });
+    res.status(201).json({ message: worker });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Server internal error", error: err });
+  }
+};
+
+module.exports.updateWorkerInfomation = async function (req, res) {
+  try {
+    const { id } = req.params;
+    const { descripcionCorta, descripcionLarga, categoria, tituloTrabajo } =
+      req.body;
+    if (
+      descripcionCorta == "" ||
+      descripcionLarga == "" ||
+      tituloTrabajo == ""
+    ) {
+      return res.status(400).json({ message: "Falta de informacion" });
+    }
+    const worker = await Trabajador.update(
+      {
+        DescripcionCorta: descripcionCorta,
+        DescripcionLarga: descripcionLarga,
+        Categoria: categoria,
+        TituloTrabajo: tituloTrabajo,
+      },
+      { where: { Id: id } }
+    );
+    res.status(200).json({ message: "Trabajador actualizado", worker });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Server internal error", error: err });
+  }
+};
+
+module.exports.updateWorkerWorks = async function (req, res) {
+  try {
+    const { id } = req.params;
+    let { Trabajos } = await Trabajador.findOne({ where: { Id: id } });
+    Trabajos++;
+    const worker = await Trabajador.update(
+      {
+        Trabajos: Trabajos,
+      },
+      { where: { Id: id } }
+    );
+    res.status(200).json({ message: "Trabajador actualizado", worker });
   } catch (err) {
     console.log(err);
     res.status(500).json({ message: "Server internal error", error: err });
--- src/routes/trabajador.routes.js
+++ src/routes/trabajador.routes.js
@@ -1,10 +1,17 @@
 const router = require("express").Router();
 const {
-  getWorkerInfoByUserId, getAllWorkers,
+  getWorkerInfoByUserId,
+  getAllWorkers,
+  addWorkerInformation,
+  updateWorkerInfomation,
+  updateWorkerWorks,
 } = require("../controllers/trabajador.controller");
 const { isAuthorized } = require("../middlewares/isAuthorized");
 
-router.get("/",getAllWorkers);
-router.get("/:id", getWorkerInfoByUserId);
+router.get("/", getAllWorkers);
+router.post("/", addWorkerInformation);
+router.get("/:id", isAuthorized, getWorkerInfoByUserId);
+router.put("/:id", isAuthorized, updateWorkerInfomation);
+router.put("/trabajos/:id", updateWorkerWorks);
 
 module.exports = router;
