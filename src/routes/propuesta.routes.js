50e0a914b0700ab647f533c415cf6773769c1294
Armando Prado <luis.prado@hemoeco.com>
2022-02-03 11:32:15 -0600

    fix(propuestas routes): now need authorization.

--- src/routes/propuesta.routes.js
+++ src/routes/propuesta.routes.js
@@ -9,11 +9,11 @@ const {
 } = require("../controllers/propuesta.controller");
 const { isAuthorized } = require("../middlewares/isAuthorized");
 
-router.post("/", addProposal);
-router.get("/:id", getClientePropos);
-router.get("/trabajador/:id",getWorkerPropos)
-router.put("/:id", acceptClient);
-router.put("/trabajador/:id", acceptWorker);
-router.put("/actualizar/:id",updateProposal);
+router.post("/", [isAuthorized], addProposal);
+router.get("/:id", [isAuthorized], getClientePropos);
+router.get("/trabajador/:id", [isAuthorized], getWorkerPropos);
+router.put("/:id", [isAuthorized], acceptClient);
+router.put("/trabajador/:id", [isAuthorized], acceptWorker);
+router.put("/actualizar/:id", [isAuthorized], updateProposal);
 
 module.exports = router;
